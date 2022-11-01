f5e1ed5d92885c8c84382fdd4d4d09db2deec15a
Marcin Bieniek <marcingim@gmail.com>
2022-11-01 21:17:12 +0100

    Add passport feature to server.js file

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+// Google API authorization
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
 const app = express();
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
@@ -12,6 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+passport.use(new GoogleStrategy({
+  clientID: "515479252272-96th68jc6rn8lh5na1lmp9cjduskq7t3.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "http://localhost:8000/auth/google/callback",
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
